91c4322f87f89607e5da940ffce7d7105b698c85
Sergey <s01denisov@gmail.com>
2022-03-26 10:14:46 +0300

    Initial commit

---
+++ app/src/main/java/com/example/myservicestest/MainActivity.kt
@@ -0,0 +1,18 @@
+package com.example.myservicestest
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.myservicestest.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private val binding by  lazy {
+        ActivityMainBinding.inflate(layoutInflater)
+    }
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(binding.root)
+    }
+}
