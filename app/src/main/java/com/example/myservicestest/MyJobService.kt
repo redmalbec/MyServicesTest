acb5ab12f61d5eceba22ce36a1e882048dc055e2
Sergey <s01denisov@gmail.com>
2022-03-27 13:54:21 +0300

    Метод enqueue

--- app/src/main/java/com/example/myservicestest/MyJobService.kt
+++ app/src/main/java/com/example/myservicestest/MyJobService.kt
@@ -5,11 +5,12 @@ import android.app.job.JobParameters
 import android.app.job.JobService
 import android.content.Context
 import android.content.Intent
+import android.os.Build
 import android.os.IBinder
 import android.util.Log
 import kotlinx.coroutines.*
 
-class MyJobService: JobService() {
+class MyJobService : JobService() {
 
     private val coroutineScope = CoroutineScope(Dispatchers.Main)
 
@@ -20,12 +21,21 @@ class MyJobService: JobService() {
 
     override fun onStartJob(params: JobParameters?): Boolean {
         log("onStartCommand")
-        coroutineScope.launch {
-            for (i in 0 until 100) {
-                delay(1000)
-                log("Timer $i")
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            coroutineScope.launch {
+                var workItem = params?.dequeueWork()
+                while (workItem != null) {
+                    val page = workItem.intent.getIntExtra(PAGE, 0)
+
+                    for (i in 0 until 5) {
+                        delay(1000)
+                        log("Timer $i $page")
+                    }
+                    params?.completeWork(workItem)
+                    workItem = params?.dequeueWork()
+                }
+                jobFinished(params, false)
             }
-            jobFinished(params, true)
         }
         return true
     }
@@ -42,11 +52,18 @@ class MyJobService: JobService() {
     }
 
     private fun log(message: String) {
-        Log.d("SERVICE_TAG", "MyService: $message")
+        Log.d("SERVICE_TAG", "MyJobService: $message")
     }
+
     companion object {
+
         const val JOB_ID = 111
+        private const val PAGE = "page"
+
+        fun newIntent(page: Int): Intent {
+            return Intent().apply {
+                putExtra(PAGE, page)
+            }
+        }
     }
-
-
 }
