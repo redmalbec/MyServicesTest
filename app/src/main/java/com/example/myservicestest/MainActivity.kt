597f307f40ab85b5f02efe9ced160a5871acc8dc
Sergey <s01denisov@gmail.com>
2022-03-27 15:57:38 +0300

    JobIntentService

--- app/src/main/java/com/example/myservicestest/MainActivity.kt
+++ app/src/main/java/com/example/myservicestest/MainActivity.kt
@@ -56,5 +56,8 @@ class MainActivity : AppCompatActivity() {
                 startService(MyIntentService2.newIntent(this, page++))
             }
         }
+        binding.jobIntentService.setOnClickListener {
+            MyJobIntentService.enqueue(this, page++)
+        }
     }
 }
